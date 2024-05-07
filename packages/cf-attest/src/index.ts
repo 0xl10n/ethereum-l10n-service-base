c0813b09c1e46b40bdfca62f1b8b12af9dee37a9
debuggingfuture <lauchunyin@debuggingfuture.com>
2024-05-07 22:20:54 +0800

    fix: cleanup

--- packages/cf-attest/src/index.ts
+++ packages/cf-attest/src/index.ts
@@ -20,29 +20,6 @@ export interface Env {
   ALCHEMY_API_KEY: string;
   ENVIRONMENT: string;
 }
-// const TRANSLATION_ATTESTATION_SCHEMA =
-//   'string sourceId, string sourceStringId, string translatedStringId, uint8 score, uint8 version';
-// const encodeTranslationAttestationSchema = (data) => {
-//   const schemaEncoder = new SchemaEncoder(TRANSLATION_ATTESTATION_SCHEMA);
-
-//   const {
-//     sourceId,
-//     sourceStringId,
-//     translatedStringId,
-//     score,
-//     version = 1,
-//   } = data;
-
-//   console.log(data);
-
-//   return schemaEncoder.encodeData([
-//     { name: 'sourceId', value: sourceId, type: 'string' },
-//     { name: 'sourceStringId', value: sourceStringId, type: 'string' },
-//     { name: 'translatedStringId', value: translatedStringId, type: 'string' },
-//     { name: 'score', value: score, type: 'uint8' },
-//     { name: 'version', value: version, type: 'uint8' },
-//   ]);
-// };
 
 const TRANSLATION_SCHEMA_UID =
   '0xdcf13573c96f6ea7bc355d4a7289bf43db18eb2dac6679e12cb764abcbb264a6';
